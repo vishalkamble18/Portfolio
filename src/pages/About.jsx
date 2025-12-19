3e8e89950f21968a24b866f07a61764a2d0ea278
Vishal Kamble <vishalkamble38396@gmail.com>
2025-12-19 11:23:24 +0530

    Netlify deploy config

--- src/pages/About.jsx
+++
@@ -1,4 +0,0 @@
-function About() {
-  return <h2>About Our Company</h2>;
-}
-export default About;
--- src/pages/Home.jsx
+++
@@ -1,4 +0,0 @@
-function Home() {
-  return <h2>Welcome to Home Page</h2>;
-}
-export default Home;
